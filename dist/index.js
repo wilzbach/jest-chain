4c8c7f2ccced5168141a6c7e8f1d34932b6e3ef2
Sebastian Wilzbach <wilzbach@bryter.io>
2021-01-28 16:33:47 +0100

    Add dist

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _chain = _interopRequireDefault(require('./chain'));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+global.expect = (0, _chain.default)(global.expect);
